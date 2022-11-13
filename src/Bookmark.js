acea224d2e2ad2bcf38ffa31d4dc4749f7a40be5
yanlinpai <yanlin@shweproperty.com>
2022-11-13 23:17:52 +0630

    update live css add section

---
+++ src/Bookmark.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Logo_the_peaks from './Logo_White.png';
+
+function Bookmark() {
+
+  
+  return (
+    
+    <div className="bookmark_section"> 
+          
+         <div>
+         Top stories 
+         </div> 
+       
+          <div className="bookmark_section_right">
+            <div>
+                Bookmark 
+            </div> 
+             <div class="custom-select" >
+                <select>
+                  <option value="0">Newest first:</option>
+                  <option value="1">Oldest first </option>
+                  <option value="2">Most popular</option> 
+                </select>
+              </div>
+          </div> 
+
+    </div>
+  )
+}
+
+export default Bookmark
